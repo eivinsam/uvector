7b081f16eecfe0b9324246d004e804c8c3b6793d
Eivind Samuelsen <eivind.samuelsen@gmail.com>
2017-08-23 15:45:03 +0200

    Made use of Unit in complex.h, plus some bugfixes

--- include/uvector/complex.h
+++ include/uvector/complex.h
@@ -28,35 +28,12 @@ namespace uv
 		template <class S> constexpr bool operator>=(Imaginary<S> b) const { return value >= b.value; }
 		template <class S> constexpr bool operator> (Imaginary<S> b) const { return value >  b.value; }
 
-		template <class S, class = if_scalar_t<S>> constexpr friend Imaginary<type::mul<T, S>> operator*(Imaginary i, S r) { return { i.value * r }; }
-		template <class S, class = if_scalar_t<S>> constexpr friend Imaginary<type::div<T, S>> operator/(Imaginary i, S r) { return { i.value / r }; }
-		template <class S, class = if_scalar_t<S>> constexpr friend Imaginary<type::mul<S, T>> operator*(S r, Imaginary i) { return { +r * i.value }; }
-		template <class S, class = if_scalar_t<S>> constexpr friend Imaginary<type::div<S, T>> operator/(S r, Imaginary i) { return { -r / i.value }; }
+		template <class S, class = if_scalar_t<S>> constexpr friend auto operator*(Imaginary i, S r) { return Imaginary<type::mul<T, S>>{ i.value * r }; }
+		template <class S, class = if_scalar_t<S>> constexpr friend auto operator/(Imaginary i, S r) { return Imaginary<type::div<T, S>>{ i.value / r }; }
+		template <class S, class = if_scalar_t<S>> constexpr friend auto operator*(S r, Imaginary i) { return Imaginary<type::mul<S, T>>{ +r * i.value }; }
+		template <class S, class = if_scalar_t<S>> constexpr friend auto operator/(S r, Imaginary i) { return Imaginary<type::div<S, T>>{ -r / i.value }; }
 	};
-	template <>
-	struct Imaginary<void>
-	{
-		constexpr int operator+(Imaginary) const { return 2; }
-		constexpr int operator-(Imaginary) const { return 0; }
-		constexpr int operator*(Imaginary) const { return -1; }
-		constexpr int operator/(Imaginary) const { return 1; }
-
-		template <class S> friend constexpr Imaginary<S>      operator+(Imaginary, Imaginary<S> b) { return { S(1) + b.value }; }
-		template <class S> friend constexpr Imaginary<S>      operator-(Imaginary, Imaginary<S> b) { return { S(1) - b.value }; }
-		template <class S> friend constexpr           S       operator*(Imaginary, Imaginary<S> b) { return    -b.value; }
-		template <class S> friend constexpr type::div<int, S> operator/(Imaginary, Imaginary<S> b) { return 1 / b.value; }
-
-		template <class S> friend constexpr Imaginary<S>      operator+(Imaginary<S> a, Imaginary) { return { a.value + S(1) }; }
-		template <class S> friend constexpr Imaginary<S>      operator-(Imaginary<S> a, Imaginary) { return { a.value - S(1) }; }
-		template <class S> friend constexpr           S       operator*(Imaginary<S> a, Imaginary) { return -a.value; }
-		template <class S> friend constexpr type::div<int, S> operator/(Imaginary<S> a, Imaginary) { return +a.value; }
-
-		template <class S, class = if_scalar_t<S>> friend constexpr Imaginary<S>                 operator*(S r, Imaginary) { return { +r }; }
-		template <class S, class = if_scalar_t<S>> friend constexpr Imaginary<S>                 operator/(S r, Imaginary) { return { -r }; }
-		template <class S, class = if_scalar_t<S>> friend constexpr Imaginary<S>                 operator*(Imaginary, S r) { return { +r }; }
-		template <class S, class = if_scalar_t<S>> friend constexpr Imaginary<type::div<int, S>> operator/(Imaginary, S r) { return { 1 / r }; }
-	};
-	static constexpr Imaginary<void> imaginary = {};
+	static constexpr Imaginary<Unit> imaginary = {};
 
 	template <class T>
 	class Complex
@@ -69,12 +46,12 @@ namespace uv
 
 		constexpr Complex operator-() const { return { -re, -im }; }
 
-		template <class S, class = if_scalar_t<S>> friend constexpr Complex<type::add<T, S>> operator+(const Complex& c, S r) { return { c.re + r, c.im }; }
-		template <class S, class = if_scalar_t<S>> friend constexpr Complex<type::add<T, S>> operator-(const Complex& c, S r) { return { c.re - r, c.im }; }
-		template <class S, class = if_scalar_t<S>> friend constexpr Complex<type::mul<T, S>> operator*(const Complex& c, S r) { return { c.re * r, c.im * r }; }
-		template <class S, class = if_scalar_t<S>> friend constexpr Complex<type::div<T, S>> operator/(const Complex& c, S r) { return { c.re / r, c.im / r }; }
-		template <class S, class = if_scalar_t<S>> friend constexpr Complex<type::add<T, S>> operator-(S r, const Complex& c) { return { r - c.re, -c.im }; }
-		template <class S, class = if_scalar_t<S>> friend constexpr Complex<type::div<T, S>> operator/(S r, const Complex& c) { return (r*conjugate(c)) / square(c); }
+		template <class S, class = if_scalar_t<S>> friend constexpr auto operator+(const Complex& c, S r) { return Complex<type::add<T, S>>{ c.re + r, c.im }; }
+		template <class S, class = if_scalar_t<S>> friend constexpr auto operator-(const Complex& c, S r) { return Complex<type::add<T, S>>{ c.re - r, c.im }; }
+		template <class S, class = if_scalar_t<S>> friend constexpr auto operator*(const Complex& c, S r) { return Complex<type::mul<T, S>>{ c.re * r, c.im * r }; }
+		template <class S, class = if_scalar_t<S>> friend constexpr auto operator/(const Complex& c, S r) { return Complex<type::div<T, S>>{ c.re / r, c.im / r }; }
+		template <class S, class = if_scalar_t<S>> friend constexpr auto operator-(S r, const Complex& c) { return Complex<type::add<T, S>>{ r - c.re, -c.im }; }
+		template <class S, class = if_scalar_t<S>> friend constexpr auto operator/(S r, const Complex& c) { return Complex<type::div<T, S>>{ (r*conjugate(c)) / square(c) }; }
 
 		template <class S> constexpr Complex<type::add<T, S>> operator+(const Complex<S>& c) const { return { re + c.re, im + c.im }; }
 		template <class S> constexpr Complex<type::add<T, S>> operator-(const Complex<S>& c) const { return { re - c.re, im - c.im}; }
@@ -86,14 +63,6 @@ namespace uv
 	template <class A, class B, class = if_scalar_t<A>> constexpr Complex<type::common<A, B>> operator-(A re, Imaginary<B> im) { return { re, -im }; }
 	template <class A, class B, class = if_scalar_t<B>> constexpr Complex<type::common<A, B>> operator+(Imaginary<A> im, B re) { return { +re, im }; }
 	template <class A, class B, class = if_scalar_t<B>> constexpr Complex<type::common<A, B>> operator-(Imaginary<A> im, B re) { return { -re, im }; }
-
-	template <class S, class = if_scalar_t<S>> constexpr Complex<S> operator+(S s, Imaginary<void>) { return { s, {S(+1)} }; }
-	template <class S, class = if_scalar_t<S>> constexpr Complex<S> operator-(S s, Imaginary<void>) { return { s, {S(-1)} }; }
-	template <class S, class = if_scalar_t<S>> constexpr Complex<S> operator+(Imaginary<void>, S s) { return { +s, {S(1)} }; }
-	template <class S, class = if_scalar_t<S>> constexpr Complex<S> operator-(Imaginary<void>, S s) { return { -s, {S(1)} }; }
-
-	template <class A, class = if_scalar_t<A>>
-	auto rotation(A angle) { using namespace std; return cos(angle) + imaginary*sin(angle); }
 
 	using floatc  = Complex<float>;
 	using doublec = Complex<double>;
